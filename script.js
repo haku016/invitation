7a5b8ce7ec8943b55aa43a5b0f65f0ece5afad36
Khang Le <94455166+haku016@users.noreply.github.com>
2024-06-16 09:20:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
-const validNames1 = ['Trần Lê Như Quỳnh','Nguyễn Trương Thiên Ân', 'Đinh Quốc Paul', 'Bùi Thế Hưng', 'Lê Quốc Vinh', 'Lưu Thanh Tùng', 'Đinh Đức Lương','Chiêm Tấn Thịnh','Phan Đức Thịnh','Trần Thành Lũy','Nguyễn Hồng Liên','Nguyễn Quang Huy','Lê Ngọc Mẩn Nghi','Vương Lục Tuệ Nhi','Huỳnh Ngọc Bảo Châu','Hồ Tấn Thức','Vũ Thị Ngọc Anh','Nguyễn Thái Bình', 'Trần Nguyễn Như Lộc','Trương Thành Hưng','Trương Thị Minh Thư','Võ Yến Vy','Đỗ Hoàng Phương Thảo','Hà Thu Thảo','Nguyễn Trọng Tín','Lê Thiên Hải'];
-const validNames2 = ['Hồ Lê Đoan Thục','Donthut dth s1tg','vk PCY','donthut','eiridy','morning','Cụt','Mít','nyc','qk','bibo liên'];
+const validNames1 = ['Trần Lê Như Quỳnh','Phan Như Ý','Nguyễn Trương Thiên Ân', 'Đinh Quốc Paul', 'Bùi Thế Hưng', 'Lê Quốc Vinh', 'Lưu Thanh Tùng', 'Đinh Đức Lương','Chiêm Tấn Thịnh','Phan Đức Thịnh','Trần Thành Lũy','Nguyễn Hồng Liên','Nguyễn Quang Huy','Lê Ngọc Mẩn Nghi','Vương Lục Tuệ Nhi','Huỳnh Ngọc Bảo Châu','Hồ Tấn Thức','Vũ Thị Ngọc Anh','Nguyễn Thái Bình', 'Trần Nguyễn Như Lộc','Trương Thành Hưng','Trương Thị Minh Thư','Võ Yến Vy','Đỗ Hoàng Phương Thảo','Hà Thu Thảo','Nguyễn Trọng Tín','Lê Thiên Hải'];
+const validNames2 = ['Hồ Lê Đoan Thục','Donthut dth s1tg','vk PCY','donthut','eiridy','morning','Cụt','Mít','nyc','qk','bibo liên','nyp'];
 function checkName() {
     const nameInput = document.getElementById('nameInput').value.trim(); // Trim whitespace
 
