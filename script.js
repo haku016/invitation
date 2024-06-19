1298c4fa91f263f6459c08a952bec1f91204f062
Khang Le <94455166+haku016@users.noreply.github.com>
2024-06-19 19:26:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 const validNames1 = ['Trần Lê Như Quỳnh','Nguyễn Lê Thiện Nhân','Phan Như Ý','Nguyễn Trương Thiên Ân', 'Đinh Quốc Paul', 'Bùi Thế Hưng', 'Lê Quốc Vinh', 'Lưu Thanh Tùng', 'Đinh Đức Lương','Nguyễn Trọng Tín','Lê Thiên Hải','Đinh Trường Luân','Đoàn Huy Tài','Chiêm Tấn Thịnh','Phan Đức Thịnh','Trần Thành Lũy','Nguyễn Hồng Liên','Nguyễn Quang Huy','Lê Ngọc Mẩn Nghi','Vương Lục Tuệ Nhi','Huỳnh Ngọc Bảo Châu','Hồ Tấn Thức','Vũ Thị Ngọc Anh','Nguyễn Thái Bình', 'Trần Nguyễn Như Lộc','Vũ Quang Việt','Đặng Phùng Đoan Thục','Nguyễn Hồng Nhung','Trương Thành Hưng','Trương Thị Minh Thư','Võ Yến Vy','Đỗ Hoàng Phương Thảo','Hà Thu Thảo','Hoàng Kim Anh'];
-const validNames2 = ['Hồ Lê Đoan Thục','Donthut dth s1tg','vk PCY','donthut','eiridy','morning','nyp'];
+const validNames2 = ['Hồ Lê Đoan Thục','Donthut dth s1tg','vk PCY','donthut','eiridy','morning','nyp','vk Sakura'];
 function checkName() {
     const nameInput = document.getElementById('nameInput').value.trim(); // Trim whitespace
 
